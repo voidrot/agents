fc7a73bef5da341d3f70a1c4ec7480421c9b92d9
Buck Brady <22723438+voidrot@users.noreply.github.com>
2026-08-23 22:53:06 -0600

    start planning eval framework

---
+++ devenv.nix
@@ -0,0 +1,65 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+  delta.enable = true;
+
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    version = "3.14";
+    uv = {
+      enable = true;
+      sync = {
+        enable = true;
+        allGroups = true;
+      };
+    };
+    lsp.enable = true;
+    lsp.package = pkgs.ty;
+  };
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    # hello         # Run scripts directly
+    # git --version # Use packages
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    # echo "Running tests"
+    # git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
