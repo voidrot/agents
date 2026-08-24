bd4df32c0822b03ff05a0ccc978fa4a029821d1c
Buck Brady <22723438+voidrot@users.noreply.github.com>
2026-08-23 22:22:34 -0600

    setup flake.nix with devenv

--- flake.nix
+++ flake.nix
@@ -1,22 +1,47 @@
 {
-  description = "Dev Env for SKILL.md Eval Tool";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            python314
-            uv
-            ruff
-          ];
-        };
-      });
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [
+                    python3
+                    uv
+                    ruff
+                    ty
+                  ];
+
+                  # enterShell = ''
+                  #   hello
+                  # '';
+
+                  # processes.hello.exec = "hello";
+                }
+              ];
+            };
+          });
+    };
 }
